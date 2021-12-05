a4025730be090103819ebf1797945e72954fa2b0
NightHz <32999626+NightHz@users.noreply.github.com>
2021-12-05 19:55:28 +0800

    update clip3d; and add clip

--- Rehenz/clipper.cpp
+++ Rehenz/clipper.cpp
@@ -39,6 +39,18 @@ namespace Rehenz
 		return state;
 	}
 
+	int ComputeClipState(Point p)
+	{
+		int state = ClipState::inside;
+		if (p.x < -p.w)     state |= ClipState::left;
+		else if (p.x > p.w) state |= ClipState::right;
+		if (p.y < -p.w)     state |= ClipState::down;
+		else if (p.y > p.w) state |= ClipState::top;
+		if (p.z < 0)        state |= ClipState::back;
+		else if (p.z > p.w) state |= ClipState::front;
+		return state;
+	}
+
 	bool ClipLine2DCohenSutherland(Point2& p1, Point2& p2, float _xmax, float _ymax)
 	{
 		return ClipLine2DCohenSutherland(p1, p2, 0, _xmax, 0, _ymax);
@@ -145,42 +157,43 @@ namespace Rehenz
 			else
 			{
 				float x = 0, y = 0, z = 0;
+				Point3 dp = p2 - p1;
 				int state = p2_state > p1_state ? p2_state : p1_state;
 				if (state & ClipState::front)
 				{
-					x = p1.x + (p2.x - p1.x) * (zmax - p1.z) / (p2.z - p1.z);
-					y = p1.y + (p2.y - p1.y) * (zmax - p1.z) / (p2.z - p1.z);
+					x = p1.x + dp.x * (zmax - p1.z) / dp.z;
+					y = p1.y + dp.y * (zmax - p1.z) / dp.z;
 					z = zmax;
 				}
 				else if (state & ClipState::back)
 				{
-					x = p1.x + (p2.x - p1.x) * (zmin - p1.z) / (p2.z - p1.z);
-					y = p1.y + (p2.y - p1.y) * (zmin - p1.z) / (p2.z - p1.z);
+					x = p1.x + dp.x * (zmin - p1.z) / dp.z;
+					y = p1.y + dp.y * (zmin - p1.z) / dp.z;
 					z = zmin;
 				}
 				else if (state & ClipState::top)
 				{
-					x = p1.x + (p2.x - p1.x) * (ymax - p1.y) / (p2.y - p1.y);
+					x = p1.x + dp.x * (ymax - p1.y) / dp.y;
 					y = ymax;
-					z = p1.z + (p2.z - p1.z) * (ymax - p1.y) / (p2.y - p1.y);
+					z = p1.z + dp.z * (ymax - p1.y) / dp.y;
 				}
 				else if (state & ClipState::down)
 				{
-					x = p1.x + (p2.x - p1.x) * (ymin - p1.y) / (p2.y - p1.y);
+					x = p1.x + dp.x * (ymin - p1.y) / dp.y;
 					y = ymin;
-					z = p1.z + (p2.z - p1.z) * (ymin - p1.y) / (p2.y - p1.y);
+					z = p1.z + dp.z * (ymin - p1.y) / dp.y;
 				}
 				else if (state & ClipState::right)
 				{
 					x = xmax;
-					y = p1.y + (p2.y - p1.y) * (xmax - p1.x) / (p2.x - p1.x);
-					z = p1.z + (p2.z - p1.z) * (xmax - p1.x) / (p2.x - p1.x);
+					y = p1.y + dp.y * (xmax - p1.x) / dp.x;
+					z = p1.z + dp.z * (xmax - p1.x) / dp.x;
 				}
 				else if (state & ClipState::left)
 				{
 					x = xmin;
-					y = p1.y + (p2.y - p1.y) * (xmin - p1.x) / (p2.x - p1.x);
-					z = p1.z + (p2.z - p1.z) * (xmin - p1.x) / (p2.x - p1.x);
+					y = p1.y + dp.y * (xmin - p1.x) / dp.x;
+					z = p1.z + dp.z * (xmin - p1.x) / dp.x;
 				}
 
 				if (state == p1_state)
@@ -191,6 +204,65 @@ namespace Rehenz
 				else
 				{
 					p2 = Point3(x, y, z);
+					p2_state = ComputeClipState(p2);
+				}
+			}
+		}
+	}
+
+	bool ClipLineCohenSutherland(Point& p1, Point& p2)
+	{
+		int p1_state = ComputeClipState(p1);
+		int p2_state = ComputeClipState(p2);
+
+		while (true)
+		{
+			if (p1_state == ClipState::inside && p2_state == ClipState::inside)
+			{
+				return true;
+			}
+			else if (p1_state & p2_state)
+			{
+				return false;
+			}
+			else
+			{
+				Point p;
+				Point dp = p2 - p1;
+				int state = p2_state > p1_state ? p2_state : p1_state;
+				if (state & ClipState::front)
+				{
+					p = p1 + dp * ((p1.w - p1.z) / (dp.z - dp.w));
+				}
+				else if (state & ClipState::back)
+				{
+					p = p1 + dp * (-p1.z / dp.z);
+				}
+				else if (state & ClipState::top)
+				{
+					p = p1 + dp * ((p1.w - p1.y) / (dp.y - dp.w));
+				}
+				else if (state & ClipState::down)
+				{
+					p = p1 + dp * ((-p1.w - p1.y) / (dp.y + dp.w));
+				}
+				else if (state & ClipState::right)
+				{
+					p = p1 + dp * ((p1.w - p1.x) / (dp.x - dp.w));
+				}
+				else if (state & ClipState::left)
+				{
+					p = p1 + dp * ((-p1.w - p1.x) / (dp.x + dp.w));
+				}
+
+				if (state == p1_state)
+				{
+					p1 = p;
+					p1_state = ComputeClipState(p1);
+				}
+				else
+				{
+					p2 = p;
 					p2_state = ComputeClipState(p2);
 				}
 			}
--- Rehenz/clipper.h
+++ Rehenz/clipper.h
@@ -3,9 +3,18 @@
 
 namespace Rehenz
 {
+	// inside return 0
+	int ComputeClipState(Point2 p);
+	// inside return 0
+	int ComputeClipState(Point3 p);
+	// inside return 0
+	int ComputeClipState(Point p);
+
 	bool ClipLine2DCohenSutherland(Point2& p1, Point2& p2, float _xmax, float _ymax);
 	bool ClipLine2DCohenSutherland(Point2& p1, Point2& p2, float _xmin, float _xmax, float _ymin, float _ymax);
 
 	bool ClipLine3DCohenSutherland(Point3& p1, Point3& p2, float _xmax, float _ymax, float _zmax);
 	bool ClipLine3DCohenSutherland(Point3& p1, Point3& p2, float _xmin, float _xmax, float _ymin, float _ymax, float _zmin, float _zmax);
+
+	bool ClipLineCohenSutherland(Point& p1, Point& p2);
 }
