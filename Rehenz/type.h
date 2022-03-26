cde6d6d67712c7237112b1b8a5dfed508f30b9fc
NightHz <32999626+NightHz@users.noreply.github.com>
2022-03-26 20:21:30 +0800

    update type

--- Rehenz/type.h
+++ Rehenz/type.h
@@ -7,6 +7,12 @@ namespace Rehenz
 	typedef unsigned char uchar;
 	typedef long long llong;
 	typedef unsigned long long ullong;
+
+	typedef unsigned char byte;
+	typedef unsigned short word;
+	typedef unsigned long dword;
+	typedef unsigned long long qword;
+
 #ifdef UNICODE
 	typedef std::wstring String;
 #else
